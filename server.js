ad8d331a811c7ad5d7cb9942d76d81c67124fd90
Ruslan Shashkov <rshashkov@icloud.com>
2015-01-14 12:14:07 +0300

    w

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(5000);
+connect().use(serveStatic(__dirname)).listen(process.env.PORT || 5000);
